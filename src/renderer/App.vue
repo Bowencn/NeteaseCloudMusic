bb0462fa5e5fc1dad539e363cac2a92457b67ee1
bowen <ousttiiaa@outlook.com>
2019-05-22 23:05:51 +0800

    new

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'neteasecloudmusic'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
